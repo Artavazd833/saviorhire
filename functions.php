61c39ebeda485d733fe7ed49d970d62937fd19b7
Atanesyan Artavazd <artavazd@sunrisedvp.com>
2023-11-07 01:46:27 +0400

    bari gisher barekamner

--- functions.php
+++ functions.php
@@ -8,55 +8,28 @@
  */
 
 if ( ! defined( '_S_VERSION' ) ) {
-	// Replace the version number of the theme on each release.
 	define( '_S_VERSION', '1.0.0' );
 }
 
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
+
 function tenweb_setup() {
-	/*
-		* Make theme available for translation.
-		* Translations can be filed in the /languages/ directory.
-		* If you're building a theme based on tenweb, use a find and replace
-		* to change 'tenweb' to the name of your theme in all the template files.
-		*/
+	
 	load_theme_textdomain( 'tenweb', get_template_directory() . '/languages' );
 
-	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
 
-	/*
-		* Let WordPress manage the document title.
-		* By adding theme support, we declare that this theme does not use a
-		* hard-coded <title> tag in the document head, and expect WordPress to
-		* provide it for us.
-		*/
 	add_theme_support( 'title-tag' );
 
-	/*
-		* Enable support for Post Thumbnails on posts and pages.
-		*
-		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		*/
 	add_theme_support( 'post-thumbnails' );
 
-	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
 			'menu-1' => esc_html__( 'Primary', 'tenweb' ),
+			'menu-2' => esc_html__( 'Footer menu', 'tenweb' ),
+			
 		)
 	);
 
-	/*
-		* Switch default core markup for search form, comment form, and comments
-		* to output valid HTML5.
-		*/
 	add_theme_support(
 		'html5',
 		array(
@@ -70,7 +43,6 @@ function tenweb_setup() {
 		)
 	);
 
-	// Set up the WordPress core custom background feature.
 	add_theme_support(
 		'custom-background',
 		apply_filters(
@@ -82,14 +54,8 @@ function tenweb_setup() {
 		)
 	);
 
-	// Add theme support for selective refresh for widgets.
 	add_theme_support( 'customize-selective-refresh-widgets' );
 
-	/**
-	 * Add support for core custom logo.
-	 *
-	 * @link https://codex.wordpress.org/Theme_Logo
-	 */
 	add_theme_support(
 		'custom-logo',
 		array(
@@ -102,23 +68,11 @@ function tenweb_setup() {
 }
 add_action( 'after_setup_theme', 'tenweb_setup' );
 
-/**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
 function tenweb_content_width() {
 	$GLOBALS['content_width'] = apply_filters( 'tenweb_content_width', 640 );
 }
 add_action( 'after_setup_theme', 'tenweb_content_width', 0 );
 
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
 function tenweb_widgets_init() {
 	register_sidebar(
 		array(
@@ -134,9 +88,7 @@ function tenweb_widgets_init() {
 }
 add_action( 'widgets_init', 'tenweb_widgets_init' );
 
-/**
- * Enqueue scripts and styles.
- */
+
 function tenweb_scripts() {
 	wp_enqueue_style( 'tenweb-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'tenweb-style', 'rtl', 'replace' );
@@ -149,29 +101,11 @@ function tenweb_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'tenweb_scripts' );
 
-/**
- * Implement the Custom Header feature.
- */
 require get_template_directory() . '/inc/custom-header.php';
-
-/**
- * Custom template tags for this theme.
- */
 require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Functions which enhance the theme by hooking into WordPress.
- */
 require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Customizer additions.
- */
 require get_template_directory() . '/inc/customizer.php';
 
-/**
- * Load Jetpack compatibility file.
- */
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
--- header.php
+++ header.php
@@ -23,27 +23,13 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'tenweb' ); ?></a>
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$tenweb_description = get_bloginfo( 'description', 'display' );
-			if ( $tenweb_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $tenweb_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+			the_custom_logo();?>
+			
+			
 
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'tenweb' ); ?></button>
@@ -52,6 +38,7 @@
 				array(
 					'theme_location' => 'menu-1',
 					'menu_id'        => 'primary-menu',
+					'depth' => 2,
 				)
 			);
 			?>
