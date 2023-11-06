d775589b8e827054813bb0ccd68842ca0a530ea8
Atanesyan Artavazd <artavazd@sunrisedvp.com>
2023-11-06 12:14:41 +0400

    com

--- functions.php
+++ functions.php
@@ -8,55 +8,24 @@
  */
 
 if ( ! defined( '_S_VERSION' ) ) {
-	// Replace the version number of the theme on each release.
 	define( '_S_VERSION', '1.0.0' );
 }
-
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
 function tenweb_setup() {
-	/*
-		* Make theme available for translation.
-		* Translations can be filed in the /languages/ directory.
-		* If you're building a theme based on tenweb, use a find and replace
-		* to change 'tenweb' to the name of your theme in all the template files.
-		*/
+	
 	load_theme_textdomain( 'tenweb', get_template_directory() . '/languages' );
 
-	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
 
-	/*
-		* Let WordPress manage the document title.
-		* By adding theme support, we declare that this theme does not use a
-		* hard-coded <title> tag in the document head, and expect WordPress to
-		* provide it for us.
-		*/
 	add_theme_support( 'title-tag' );
 
-	/*
-		* Enable support for Post Thumbnails on posts and pages.
-		*
-		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		*/
 	add_theme_support( 'post-thumbnails' );
 
-	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
 			'menu-1' => esc_html__( 'Primary', 'tenweb' ),
 		)
 	);
 
-	/*
-		* Switch default core markup for search form, comment form, and comments
-		* to output valid HTML5.
-		*/
 	add_theme_support(
 		'html5',
 		array(
@@ -70,7 +39,6 @@ function tenweb_setup() {
 		)
 	);
 
-	// Set up the WordPress core custom background feature.
 	add_theme_support(
 		'custom-background',
 		apply_filters(
@@ -82,7 +50,6 @@ function tenweb_setup() {
 		)
 	);
 
-	// Add theme support for selective refresh for widgets.
 	add_theme_support( 'customize-selective-refresh-widgets' );
 
 	/**
@@ -138,10 +105,9 @@ add_action( 'widgets_init', 'tenweb_widgets_init' );
  * Enqueue scripts and styles.
  */
 function tenweb_scripts() {
-	wp_enqueue_style( 'tenweb-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'tenweb-style', 'rtl', 'replace' );
+	wp_enqueue_style( 'tenweb-style',get_template_directory_uri() . '/dist/css/style.min.css', array(), _S_VERSION );
 
-	wp_enqueue_script( 'tenweb-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'tenweb-scripts', get_template_directory_uri() . '/dist/js/all.min.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -149,9 +115,6 @@ function tenweb_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'tenweb_scripts' );
 
-/**
- * Implement the Custom Header feature.
- */
 require get_template_directory() . '/inc/custom-header.php';
 
 /**
